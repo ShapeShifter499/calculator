9f41dda64c42fcbe895bb172c142fd20e6de8a2d
Lance Geroso <Gero3977@gmail.com>
2013-04-13 07:12:20 -0700

    ignore backup files

---
+++ calculator.rb
@@ -0,0 +1,28 @@
+#HP Style calculator
+
+#User input
+
+class Calculator
+
+UserInput = gets.chomp
+
+def memory
+  memory = []
+end
+
+def maincalc
+if UserInput = q
+  puts "this appears to be working"
+  exit
+end
+
+maincalc
+
+end
+
+end
+
+end
+
+end
+
